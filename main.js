e551a56f1389a1250279b9d21a771610c6b05f2e
Aviraj19 <73528868+Aviraj19@users.noreply.github.com>
2022-02-14 16:30:57 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
 function setup() {
-    canvas = createCanvas(500,500)
-    canvas.center()
+    canvas = createCanvas(680, 500)
+    canvas.position(1100, 200)
+    video = createCapture(VIDEO)
+    video.position(50, 220)
+    poseNet = ml5.poseNet(video, modelloaded)
+    poseNet.on("pose", gotposes)
+}
+
+function modelloaded() {
+    console.log("poseNet is loaded")
+}
+
+function draw() {
+    background("gray")
+}
+
+function gotposes(results) {
+    if (results.length > 0) {
+        console.log(results)
+    }
 }
