f17f8baa9f350f88ad59ace1b36557f28633cb3f
Aviraj19 <73528868+Aviraj19@users.noreply.github.com>
2022-02-16 16:38:43 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+nosex = 0 
+nosey = 0 
+leftwrist_x = 0
+rightwrist_x = 0
+difference = 0 
 function setup() {
     canvas = createCanvas(680, 500)
     canvas.position(1100, 200)
@@ -11,12 +16,19 @@ function modelloaded() {
     console.log("poseNet is loaded")
 }
 
-function draw() {
-    background("gray")
-}
-
 function gotposes(results) {
     if (results.length > 0) {
         console.log(results)
+        nosex = results[0].pose.nose.x
+        nosey = results[0].pose.nose.y
+        leftwrist_x =  results[0].pose.leftWrist.x
+        rightwrist_x =  results[0].pose.rightWrist.x
+        difference = floor(leftwrist_x - rightwrist_x)
     }
+}
+function draw() {
+    background("gray")
+    fill("red")
+    square(nosex,nosey,difference)
+    document.getElementById("sqauresides").innerHTML="length of the square is"+ difference
 }
